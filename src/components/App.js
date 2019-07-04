18660129d66ce1cb8b1664a254c1e20b526453c8
kowalus23 <kowalus23@gmail.com>
2019-07-04 18:31:04 +0200

    making basic validation for create form

--- src/components/App.js
+++ src/components/App.js
@@ -15,10 +15,10 @@ const App = () => {
         <Header/>
         <div>
           <Route path={'/'} exact component={StreamList}/>
-          <Route path={'/stream/new'} component={StreamCreate}/>
-          <Route path={'/stream/edit'} component={StreamEdit}/>
-          <Route path={'/stream/delete'} component={StreamDelete}/>
-          <Route path={'/stream/show'} component={StreamShow}/>
+          <Route path={'/stream/new'} exact component={StreamCreate}/>
+          <Route path={'/stream/edit'} exact component={StreamEdit}/>
+          <Route path={'/stream/delete'} exact component={StreamDelete}/>
+          <Route path={'/stream/show'} exact component={StreamShow}/>
         </div>
       </BrowserRouter>
     </div>
