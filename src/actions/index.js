3db89529f1b96b2872e3753e9c180193f5eb7cd2
kowalus23 <kowalus23@gmail.com>
2019-07-04 17:02:53 +0200

    installed redux-form + added REDUX DEVTOOL to the project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import {SIGN_IN, SIGN_OUT} from "./types";
 
-export const signIn = () => {
+export const signIn = (userId) => {
   return {
     type: SIGN_IN,
-  }
+    payload: userId,
+  };
 };
 
 export const signOut = () => {
