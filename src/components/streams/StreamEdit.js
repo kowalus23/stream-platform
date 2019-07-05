e2ef68642dd883d768278e6e181fae33afb72f91
kowalus23 <kowalus23@gmail.com>
2019-07-05 18:20:52 +0200

    HUGE refactor, moved out form for standalone component and reuse it in Creat Form, then will pass it to Edit Form

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import {editStream, fetchStream} from '../../actions'
+import {connect} from "react-redux";
 
-const StreamEdit = (props) => {
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
-  console.log(props)
-  return <div>StreamEdit</div>
+  renderStream() {
+    if (!this.props.stream) {
+      return <div>Loading..</div>
+    }
+
+    return (
+      <div>
+        <h2>Title: {this.props.stream.title}</h2>
+        <h3>Description: {this.props.stream.description}</h3>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>{this.renderStream()}</div>
+    )
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {stream: state.streams[ownProps.match.params.id]}
 };
 
-export default StreamEdit
+export default connect(mapStateToProps, {editStream, fetchStream})(StreamEdit);
