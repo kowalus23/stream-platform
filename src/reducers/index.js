d54e494b48a9a1b7b6f6914300b91a09c49eb96a
kowalus23 <kowalus23@gmail.com>
2019-07-05 13:58:15 +0200

    filled reducer with whole switch for or STREAM cases + added lodash to make it easier

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import {combineReducers} from "redux";
 import {reducer as fromReducer} from 'redux-form';
 import authReducer from "./authReducer";
+import streamReducer from "./streamsReducer"
 
 export default combineReducers({
   auth: authReducer,
