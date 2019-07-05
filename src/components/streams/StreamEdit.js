4406cf5ed101ed9b7c234577067eb43da6d91de9
kowalus23 <kowalus23@gmail.com>
2019-07-05 19:07:56 +0200

    built edit component, fixed issue with REST api, by changing PUT to PATCH (because we're changing few properties, not all of them

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,21 +1,28 @@
+import _ from 'lodash';
 import React from 'react';
 import {editStream, fetchStream} from '../../actions'
 import {connect} from "react-redux";
+import StreamForm from './StreamForm'
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues)
+  };
+
   renderStream() {
     if (!this.props.stream) {
       return <div>Loading..</div>
     }
-
     return (
       <div>
-        <h2>Title: {this.props.stream.title}</h2>
-        <h3>Description: {this.props.stream.description}</h3>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}/>
       </div>
     )
   }
